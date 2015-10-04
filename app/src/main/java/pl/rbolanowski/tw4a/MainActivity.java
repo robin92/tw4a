658a52b52a81859452382545f1fe38d280bbcf2b
robin92 <robin92pl@gmail.com>
2015-10-04 12:17:21 +0200

    BackendConfigurator copies binary to internal storage.

--- app/src/main/java/pl/rbolanowski/tw4a/MainActivity.java
+++ app/src/main/java/pl/rbolanowski/tw4a/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
 
 public class MainActivity extends Activity {
@@ -20,16 +20,53 @@ public class MainActivity extends Activity {
 
 }
 
-class BackendConfigurator {
+interface BackendConfigurator {
+
+    public void configure();
+
+}
+
+interface BackendProvider {
+
+    public InputStream getInputStream();
+
+}
+
+class BackendConfiguratorImpl implements BackendConfigurator {
 
     private static final String BACKEND_FILENAME = "task";
 
     private Context mContext;
+    private BackendProvider mProvider;
     private File mTaskFile;
 
-    public BackendConfigurator(Context context) {
+    public BackendConfiguratorImpl(Context context, BackendProvider provider) {
         mContext = context;
+        mProvider = provider;
         mTaskFile = mContext.getFileStreamPath(BACKEND_FILENAME);
+    }
+
+    @Override
+    public void configure() {
+        try {
+            ByteArrayOutputStream backendBinary = readStream(mProvider.getInputStream());
+            FileOutputStream outputStream = mContext.openFileOutput(BACKEND_FILENAME, Context.MODE_PRIVATE);
+            backendBinary.writeTo(outputStream);
+            outputStream.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e.toString());
+        }
+    }
+
+    ByteArrayOutputStream readStream(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int oneByte = -1;
+        while ((oneByte = inputStream.read()) > 0) {
+            buffer.write(oneByte);
+        }
+        inputStream.close();
+        return buffer;
     }
 
 }
--- src/androidTest/java/pl/rbolanowski/tw4a/BackendConfiguratorTest.java
+++ src/androidTest/java/pl/rbolanowski/tw4a/BackendConfiguratorTest.java
@@ -1,26 +1,77 @@
 package pl.rbolanowski.tw4a;
 
+import android.test.MoreAsserts;
 import android.test.mock.MockContext;
+import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.lang.StringBuilder;
 
+import pl.rbolanowski.tw4a.BackendProvider;
 import pl.rbolanowski.tw4a.test.AndroidMockitoTestCase;
 
 import static org.mockito.Mockito.*;
 
 public class BackendConfiguratorTest extends AndroidMockitoTestCase {
 
+    private static final String BINARY_CONTENT = "hello world";
+
     private MockContext mContext;
+    private BackendProvider mProvider;
     private BackendConfigurator mConfigurator;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        setUpContext();
+        setUpProvider();
+        setUpConfigurator();
+    }
+
+    private void setUpContext() {
         mContext = mock(MockContext.class);
-        mConfigurator = new BackendConfigurator(mContext);
-        assertNotNull(mContext, mConfigurator);
+    }
+
+    private void setUpProvider() {
+        mProvider = mock(BackendProvider.class);
+        when(mProvider.getInputStream()).thenReturn(null);
+    }
+
+    private void setUpConfigurator() {
+        assertNotNull(mContext, mProvider);
+        mConfigurator = new BackendConfiguratorImpl(mContext, mProvider);
     }
 
     public void testGetsPathToBackendBinary() {
         verify(mContext).getFileStreamPath("task");
+    }
+
+    public void testDownloadsBackendBinary() throws Exception {
+        InputStream inputStream = new ByteArrayInputStream(BINARY_CONTENT.getBytes());
+        when(mProvider.getInputStream()).thenReturn(inputStream);
+
+        File tempFile = File.createTempFile("tw4aTest", ".txt");
+        tempFile.deleteOnExit();
+        FileOutputStream outputStream = new FileOutputStream(tempFile);
+        when(mContext.openFileOutput(anyString(), anyInt())).thenReturn(outputStream);
+
+        mConfigurator.configure();
+        verify(mProvider).getInputStream();
+        verify(mContext).openFileOutput("task", MockContext.MODE_PRIVATE);
+
+        MoreAsserts.assertEquals(BINARY_CONTENT.getBytes(), readFile(tempFile));
+    }
+
+    private byte[] readFile(File file) throws IOException {
+        FileInputStream inputStream = new FileInputStream(file);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int b = 0;
+        while ((b = inputStream.read()) >= 0) {
+            outputStream.write(b);
+        }
+        inputStream.close();
+        outputStream.close();
+        return outputStream.toByteArray();
     }
 
 }
