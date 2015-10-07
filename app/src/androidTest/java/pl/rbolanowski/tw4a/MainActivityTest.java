b376d98fd432628d026245553fd066b6e2c79d88
robin92 <robin92pl@gmail.com>
2015-10-07 08:57:09 +0200

    Testing MainActivity for configuring backend.

--- app/src/androidTest/java/pl/rbolanowski/tw4a/MainActivityTest.java
+++ app/src/androidTest/java/pl/rbolanowski/tw4a/MainActivityTest.java
@@ -3,22 +3,41 @@ package pl.rbolanowski.tw4a;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+
+import java.io.File;
 
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 
+import static org.junit.Assert.*;
+
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    @Rule public ActivityTestRule<MainActivity> mActivityRyle = new ActivityTestRule<>(MainActivity.class);
+    private MainActivity mActivity;
+    @Rule public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before public void setUp() {
+        mActivity = mActivityRule.getActivity();
+        assertNotNull(mActivity);
+    }
 
     @Test public void activityLoadsData() {
         onView(withId(android.R.id.progress)).check(matches(isEnabled()));
         onView(withId(android.R.id.text1)).check(matches(isDisplayed()));
+    }
+
+    @After public void tearDown() {
+        assertResourceReady("task");
+    }
+
+    private void assertResourceReady(String name) {
+        File file = mActivity.getFileStreamPath(name);
+        assertTrue(file.exists());
+        assertTrue(file.canExecute());
     }
 
 }
