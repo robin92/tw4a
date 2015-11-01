3a7cb8a1d1d63712e482a22b87ff5f12dc9d4790
robin92 <robin92pl@gmail.com>
2015-11-01 11:31:13 +0100

    [espresso] setting timeout for idling resources moved

--- app/src/androidTest/java/pl/rbolanowski/tw4a/test/AndroidTestCase.java
+++ app/src/androidTest/java/pl/rbolanowski/tw4a/test/AndroidTestCase.java
@@ -3,10 +3,19 @@ package pl.rbolanowski.tw4a.test;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.junit.Before;
 
+import static android.support.test.espresso.IdlingPolicies.*;
+import static pl.rbolanowski.tw4a.test.Constants.*;
+
 public class AndroidTestCase extends Assert {
+
+    static {
+        setMasterPolicyTimeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
+    }
 
     @Before public void setUpContext() {
         assertNotNull(getTargetContext());
