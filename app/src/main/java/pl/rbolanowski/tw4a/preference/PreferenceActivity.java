3d898e80ff38b7940439607177cf275f12041277
robin92 <robin92pl@gmail.com>
2016-02-18 18:55:05 +0100

    PreferenceActivity configures taskwarrior private key

--- app/src/main/java/pl/rbolanowski/tw4a/preference/PreferenceActivity.java
+++ app/src/main/java/pl/rbolanowski/tw4a/preference/PreferenceActivity.java
@@ -1,15 +1,47 @@
 package pl.rbolanowski.tw4a.preference;
 
+import android.content.Intent;
+import android.preference.Preference;
+
 import roboguice.activity.RoboPreferenceActivity;
+import roboguice.inject.InjectPreference;
 
 import pl.rbolanowski.tw4a.R;
 
-public class PreferenceActivity extends RoboPreferenceActivity {
+public class PreferenceActivity extends RoboPreferenceActivity
+    implements Preference.OnPreferenceClickListener {
+
+    private String mCurrentKey;
 
     @Override
     public void onCreate(android.os.Bundle savedState) {
         super.onCreate(savedState);
         addPreferencesFromResource(R.xml.preferences);
+
+        findPreference(getString(R.string.pref_key_taskwarrior_private_key))
+            .setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        Intent fileBrowserIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        fileBrowserIntent.setType("application/x-pem-file");
+        fileBrowserIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        mCurrentKey = preference.getKey();
+        startActivityForResult(fileBrowserIntent, 0);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent result) {
+        super.onActivityResult(requestCode, responseCode, result);
+        if (responseCode == RESULT_OK) {
+            getPreferenceManager().getSharedPreferences()
+                .edit()
+                .putString(mCurrentKey, result.getData().toString())
+                .commit();
+        }
+        mCurrentKey = null;
     }
 
 }
